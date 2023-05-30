b7e778dbd48ce1382b211de79091d02daac6db74
Rubert_G <rubertgonzalez5@gmail.com>
2023-05-30 11:40:50 -0500

    title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import Nav from '@/components/Nav'
 import Contact from '@/components/Contact'
 
 export const metadata = {
-   title: 'KevinWeb',
+   title: 'KevinWeb - Página web de Kevin Gonzalez.',
    description: 'Portafolio y blog personal del ingeniero electromecanico y especialista en seguridad y salud en el trabajo, Kevin Gonzalez.',
 }
 
